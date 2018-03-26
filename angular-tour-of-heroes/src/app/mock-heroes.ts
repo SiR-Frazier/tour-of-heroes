9879525d09f2bdb87f4352face27fcabbc33f1f8
Bryce Frazier and Nico Daunt <nico@>
2018-03-26 15:54:56 -0700

    initialize repo

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Iceman' },
+  { id: 13, name: 'The Flash' },
+  { id: 14, name: 'Galactus' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Colossus' },
+  { id: 17, name: 'The Hulk' },
+  { id: 18, name: 'The Thing' },
+  { id: 19, name: 'Batman' },
+  { id: 20, name: 'Ironman' }
+];
